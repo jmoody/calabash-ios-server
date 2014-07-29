dcf2b622087dc016c8b866353a3ef14d61c31d56
Joshua Moody <joshuajmoody@gmail.com>
2014-07-29 05:28:00 +0200

    build-frank.sh should run xcodebuild with xcrun

--- build-frank.sh
+++ build-frank.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rm -fr build && xcodebuild -target "frank-calabash" -configuration Debug SYMROOT=build SDKROOT=iphonesimulator IPHONEOS_DEPLOYMENT_TARGET=5.1.1
+rm -fr build && xcrun xcodebuild -target "frank-calabash" -configuration Debug SYMROOT=build SDKROOT=iphonesimulator IPHONEOS_DEPLOYMENT_TARGET=5.1.1
